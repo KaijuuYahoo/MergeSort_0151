97f41e291f654acbf4c6ba8082302e5e75cb4bcd
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-04-17 18:22:47 +0700

    Penambahan Step 4 pada Procedure MergeSort

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -40,9 +40,47 @@ void MergeSort(int low, int high){
     }
     // step 2
     int mid = (low + high) / 2;
+
     // step 3
     MergeSort(low, mid);
     MergeSort(mid + 1, high);
+    
+    // step 4
+    int i, j;
+    i = low;     // step 4.a
+    j = mid + 1; // step 4.b
+
+    int k = low;                  // step 4.c
+    while (i <= mid && j <= high) // step 4.d
+    {
+        if (arr[i] <= arr[j])
+        { // step 4.d.i
+            B[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            {
+                B[k] = arr[j];
+                j++;
+            }
+            k++; // step4.d.ii
+        }
+
+        // step 4.e
+        while (j <= high)
+        {
+            B[k] = arr[j];
+            j++;
+            k++;
+        }
+        // step 4.f
+        while (i <= mid)
+        {
+            B[k] = arr[i];
+            i++;
+            k++;
+        }
 }
 
 void Output()
