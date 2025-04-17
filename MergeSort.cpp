ea604de71986e9d793fa7d89867e9425e80ab1e2
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-04-17 18:15:59 +0700

    Penambahan Procedure MergeSort

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 using namespace std;
 #include <string>
-int arr[20], B[20]; //Membuat array dengan panjang data 20
+int arr[20], B[20]; // Membuat array dengan panjang data 20
 
 int n; // membuat variable inputan n
 
-void input(){
-    while (true){
+void input()
+{
+    while (true)
+    {
         cout << "Masukkan Panjang Element Array : ";
         cin >> n;
 
-        if(n <= 20){
+        if (n <= 20)
+        {
             break;
         }
-        else{
+        else
+        {
             cout << "Inputan Element Maksimal 20";
         }
 
@@ -21,25 +25,26 @@ void input(){
         cout << "\nInputan Isi Element Array";
         cout << "\n=========================";
 
-        for ( int i=0 ; i<n ; i++ ){
+        for (int i = 0; i < n; i++)
+        {
             cout << "Array Index Ke-" << i << " : ";
             cin >> arr[i];
         }
     }
-};
-void MergeSort(int low,int high){
-    
 }
+void MergeSort(int low, int high)
 
-void Output(){
-    cout << "\n Data setelah diurutkan (Merge Sort) : ";
-    for( int i = 0; i < n ; i++ ){
+void Output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
 
-int main(){
-    input();
-    Output();
+int main()
+{
+
 }
